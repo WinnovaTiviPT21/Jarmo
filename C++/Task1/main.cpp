f9f1af29a71c502d4b6bc264b3c99ff769915f4d
Maahantuoja <122344190+Maahantuoja@users.noreply.github.com>
2023-01-10 13:20:43 +0200

    Task 1.1
    Kolmas päivitys

--- C++/Task1/main.cpp
+++ C++/Task1/main.cpp
@@ -2,26 +2,24 @@
 
 using namespace std;
 
-int kysymys()
+int kysymys(string txt)
 {
+    cout << txt;
     int luku;
-    cout << "Anna luku: ";
     cin >> luku;
     return luku;
 }
 
 void tulostus(int x, int y)
 {
-    int summa = x + y;
-    int erotus = x - y;
-    cout << "Antamiesi lukujen summa on: " << summa << endl;
-    cout << "Antamiesi lukujen erotus on: " << erotus << endl;
+    cout << "Antamiesi lukujen summa on: " << x + y << endl;
+    cout << "Antamiesi lukujen erotus on: " << x - y << endl;
 }
 
 int main()
 {
-    int x = kysymys();
-    int y = kysymys();
+    int x = kysymys("Anna luku: ");
+    int y = kysymys("Anna toinen luku: ");
 
     tulostus(x, y);
     return 0;
