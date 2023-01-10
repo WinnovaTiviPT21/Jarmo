2d2cc3c8d944a5055ed38c77b0ccf6638ac12723
Maahantuoja <122344190+Maahantuoja@users.noreply.github.com>
2023-01-10 14:43:19 +0200

    Task2.1
    Ohjelma kysyy vuosilukua ja kertoo onko kyseessä karkausvuosi.

--- C++/Task2/main.cpp
+++ C++/Task2/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-#include "io.h"
 
 using namespace std;
 
 int main()
 {
+    int vuosi = 0;
+    cout << "Anna vuosiluku: ";
+    cin >> vuosi;
+
+    if(vuosi % 400 == 0)
+    {
+        cout << "Kyseessa on karkausvuosi\n";
+    }
+    else if(vuosi % 100 == 0)
+    {
+        cout << "Kyseessa ei ole karkausvuosi\n";
+    }
+    else if(vuosi % 4 == 0)
+    {
+        cout << "Kyseessa on karkausvuosi\n";
+    }
+    else
+    {
+        cout << "Kyseessa ei ole karkausvuosi\n";
+    }
     return 0;
 }
