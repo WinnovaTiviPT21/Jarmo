5d9993337fb5e89968fdee8cdd17d6429159fede
Maahantuoja <122344190+Maahantuoja@users.noreply.github.com>
2023-01-10 15:22:57 +0200

    Task2.2
    Tee ohjelma, joka kysyy päivämäärän suomalaisessa esitysmuodossa (11.11.2021).
    
    Tämä ei ollut haettu ratkaisu.

--- C++/Task2/main.cpp
+++ C++/Task2/main.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
-    int vuosi = 0;
+    time_t now = time(0);
+    char* dt = ctime(&now);
+    cout << "The local date and time is: " << dt << endl;
+    tm *gmtm = gmtime(&now);
+    dt = asctime(gmtm);
+    /*int pv;
+    int kk;
+    int vuosi;
+
+    cout << "Anna paivamaara: ";
+    cin >> pv;
+    if(pv < 1 || pv > 31)
+    {
+        cout << "Antamasi paivamaara on virheellinen!\n";
+    }
+
+    cout << "Anna kuukausi: ";
+    cin >> kk;
+    if(kk < 1 || kk > 12)
+    {
+        cout << "Antamasi kuukausi on virheellinen!\n";
+    }
+
     cout << "Anna vuosiluku: ";
     cin >> vuosi;
 
+    cout << "Antamasi paivamara on: " << pv << "." << kk << "." << vuosi << endl;
     if(vuosi % 400 == 0)
     {
         cout << "Kyseessa on karkausvuosi\n";
@@ -22,7 +46,7 @@ int main()
     }
     else
     {
-        cout << "Kyseessa ei ole karkausvuosi\n";
-    }
+        cout << "Kyseessa ei ole karkausvuosi\n\n";
+    }*/
     return 0;
 }
