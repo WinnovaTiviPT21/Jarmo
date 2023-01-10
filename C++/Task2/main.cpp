59fbffdd81b6f48186ee855ded3ad88ef31a2ca8
Maahantuoja <122344190+Maahantuoja@users.noreply.github.com>
2023-01-10 15:23:26 +0200

    Update main.cpp

--- C++/Task2/main.cpp
+++ C++/Task2/main.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
-#include <ctime>
 
 using namespace std;
 
 int main()
 {
-    time_t now = time(0);
-    char* dt = ctime(&now);
-    cout << "The local date and time is: " << dt << endl;
-    tm *gmtm = gmtime(&now);
-    dt = asctime(gmtm);
     /*int pv;
     int kk;
     int vuosi;
