eee26ebb8e95eab329702b9f47e0df5f9eaf5924
Maahantuoja <122344190+Maahantuoja@users.noreply.github.com>
2023-01-11 15:46:02 +0200

    Task3.2
    2/3
    
    Tulostaa työntekijöiden määrän.
    
    - Tässä on push_back / employees.sizen kanssa jokin ongelma, koska ontaa kokonaismäärään yhden liikaa.
    
    Laskee miestne ja naisten lukumäärän oikein.

--- C++/Task3/main.cpp
+++ C++/Task3/main.cpp
@@ -25,24 +25,33 @@ int main()
 
     vector<Employee> employees;
 
-    // int counter{0};
+    int f{0};
+    int m{0};
+    int maara{0};
     // While there's still stuff left to read
     while (inf)
     {
         // read stuff from the file into a string and print it
         string strInput;
         getline(inf, strInput);
-        //cout << strInput << '\n';
 
         int comma1 = strInput.find(',');
         int comma2 = strInput.find(',', comma1+1);
-        int comma4 = strInput.find(',', comma2+3);
+        //int comma4 = strInput.find(',', comma2+3);
 
         Employee e;
         e.m_etunimi = strInput.substr(0, comma1);
         e.m_sukunimi = strInput.substr(comma1 + 1, comma2 - comma1 - 1);
         e.m_sukupuoli = strInput.substr(comma2 + 1, 1);
-
+        if(e.m_sukupuoli == "M"){
+            m = m + 1;
+        }
+        if(e.m_sukupuoli == "F"){
+            f = f + 1;
+        }
+        if(inf){
+        maara = maara + 1;
+        }
         //string sDate;
         //sDate = strInput.substr(comma2 + 3, comma4 - comma2 - 3);
         //e.birthDate() = Date(stoi)
@@ -50,7 +59,10 @@ int main()
         employees.push_back(e);
     }
 
-    cout << "Tyontekijoiden kokonais maara: " << employees.size() << endl;
+    //cout << "Tyontekijoiden kokonais maara: " << employees.size() << endl; // employees.push_back(e); lisää ylimääräisen rivin?
+    cout << "Tyontekijoiden kokonais maara: " << maara << endl;
+    cout << "Miehia: " << m << endl;
+    cout << "Naisia: " << f << endl;
 
     cout << endl;
     return 0;
