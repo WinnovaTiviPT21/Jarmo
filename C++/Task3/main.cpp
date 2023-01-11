8cf87fbcac6068f3d0df1840dfaa7a8dc27b8936
Maahantuoja <122344190+Maahantuoja@users.noreply.github.com>
2023-01-11 13:27:06 +0200

    Task3 (opettajan avustuksella)

--- C++/Task3/main.cpp
+++ C++/Task3/main.cpp
@@ -4,9 +4,15 @@
 #include <vector>
 using namespace std;
 
+class Employee{
+public:
+    string m_etunimi;
+    string m_sukunimi;
+    string m_sukupuoli;
+};
+
 int main()
 {
-    // Tämä vaihe on ok
     ifstream inf{ "test.csv" };
 
     // If we couldn't open the output file stream for reading
@@ -17,16 +23,33 @@ int main()
         return 1;
     }
 
-    //std::vector<Employee> employees;
+    vector<Employee> employees;
 
+    // int counter{0};
     // While there's still stuff left to read
     while (inf)
     {
         // read stuff from the file into a string and print it
         string strInput;
         getline(inf, strInput);
-        cout << strInput << '\n';
+        //cout << strInput << '\n';
+
+        int comma1 = strInput.find(',');
+        int comma2 = strInput.find(',', comma1+1);
+        int comma4 = strInput.find(',', comma2+3);
+
+        Employee e;
+        e.m_etunimi = strInput.substr(0, comma1);
+        e.m_sukunimi = strInput.substr(comma1 + 1, comma2 - comma1 - 1);
+        e.m_sukupuoli = strInput.substr(comma2 + 1, 1);
+
+        //string sDate;
+        //sDate = strInput.substr(comma2 + 3, comma4 - comma2 - 3);
+        //e.birthDate() = Date(stoi)
+
+        employees.push_back(e);
     }
 
+    cout << employees.size() << endl;
     return 0;
 }
