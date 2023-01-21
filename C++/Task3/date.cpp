572abfbe7d5959723b12f3b6e0fad88fd9380fd6
Maahantuoja <122344190+Maahantuoja@users.noreply.github.com>
2023-01-21 07:05:35 +0200

    Virheen korjaus karkauspäivälaskurissa

--- C++/Task3/date.cpp
+++ C++/Task3/date.cpp
@@ -23,7 +23,7 @@ bool Date::kVuosi()
     if(year % 400 == 0)
         return true;
     else if(year % 100 == 0)
-        return true;
+        return false;
     else if(year % 4 == 0)
         return true;
     return false;
