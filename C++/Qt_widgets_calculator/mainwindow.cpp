04de6feeb3105a6b983af530aa2fbf534d7f6115
Maahantuoja <122344190+Maahantuoja@users.noreply.github.com>
2023-01-26 13:34:42 +0200

    Toimiva versio

--- C++/Qt_widgets_calculator/mainwindow.cpp
+++ C++/Qt_widgets_calculator/mainwindow.cpp
@@ -1,15 +1,184 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QToolButton>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent):
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
+    connect(&m_calculator, &Calculator::displayChanged,
+            this, &MainWindow::displayUpdate);
+
     ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::displayUpdate(QString s) {
+    ui->lineEdit->setText(s);
+}
+
+
+void MainWindow::on_B0_clicked()
+{
+    m_calculator.digitClicked("0");
+}
+
+
+void MainWindow::on_B1_clicked()
+{
+    m_calculator.digitClicked("1");
+}
+
+
+void MainWindow::on_B2_clicked()
+{
+    m_calculator.digitClicked("2");
+}
+
+
+void MainWindow::on_B3_clicked()
+{
+    m_calculator.digitClicked("3");
+}
+
+
+void MainWindow::on_B4_clicked()
+{
+    m_calculator.digitClicked("4");
+}
+
+
+void MainWindow::on_B5_clicked()
+{
+    m_calculator.digitClicked("5");
+}
+
+
+void MainWindow::on_B6_clicked()
+{
+    m_calculator.digitClicked("6");
+}
+
+
+void MainWindow::on_B7_clicked()
+{
+    m_calculator.digitClicked("7");
+}
+
+
+void MainWindow::on_B8_clicked()
+{
+    m_calculator.digitClicked("8");
+}
+
+
+void MainWindow::on_B9_clicked()
+{
+    m_calculator.digitClicked("9");
+}
+
+
+void MainWindow::on_Backspace_clicked()
+{
+    m_calculator.backspaceClicked();
+}
+
+
+void MainWindow::on_Clear_clicked()
+{
+    m_calculator.clear();
+}
+
+
+void MainWindow::on_ClearAll_clicked()
+{
+    m_calculator.clearAll();
+}
+
+
+void MainWindow::on_Plus_clicked()
+{
+    m_calculator.additiveOperatorClicked(Calculator::Addition);
+}
+
+
+void MainWindow::on_Pilkku_clicked()
+{
+    m_calculator.pointClicked();
+}
+
+
+void MainWindow::on_Miinus_clicked()
+{
+    m_calculator.additiveOperatorClicked(Calculator::Subtraction);
+}
+
+
+void MainWindow::on_Kerto_clicked()
+{
+    m_calculator.multiplicativeOperatorClicked(Calculator::Multiplication);
+}
+
+
+void MainWindow::on_Jako_clicked()
+{
+    m_calculator.multiplicativeOperatorClicked(Calculator::Division);
+}
+
+void MainWindow::on_Onyhtakuin_clicked()
+{
+    m_calculator.equalClicked();
+}
+
+
+void MainWindow::on_PlusMiinus_clicked()
+{
+    m_calculator.changeSignClicked();
+}
+
+
+void MainWindow::on_Neliojuuri_clicked()
+{
+    m_calculator.unaryOperatorClicked(Calculator::SquareRoot);
+}
+
+
+void MainWindow::on_x2_clicked()
+{
+    m_calculator.unaryOperatorClicked(Calculator::Square);
+}
+
+
+void MainWindow::on_YksiKauttaX_clicked()
+{
+    m_calculator.unaryOperatorClicked(Calculator::Inverse);
+}
+
+
+void MainWindow::on_MC_clicked()
+{
+    m_calculator.clearMemory();
+}
+
+
+void MainWindow::on_MR_clicked()
+{
+    m_calculator.readMemory();
+}
+
+
+void MainWindow::on_MS_clicked()
+{
+    m_calculator.setMemory();
+}
+
+
+void MainWindow::on_Mplus_clicked()
+{
+    m_calculator.addToMemory();
 }
 
--- C++/Qt_widgets_calculator/mainwindow.h
+++ C++/Qt_widgets_calculator/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "calculator.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +16,65 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_Backspace_clicked();
+    void displayUpdate(QString s);
+
+    void on_B1_clicked();
+
+    void on_B0_clicked();
+
+    void on_B2_clicked();
+
+    void on_B3_clicked();
+
+    void on_B4_clicked();
+
+    void on_B5_clicked();
+
+    void on_B6_clicked();
+
+    void on_B7_clicked();
+
+    void on_B8_clicked();
+
+    void on_B9_clicked();
+
+    void on_Clear_clicked();
+
+    void on_ClearAll_clicked();
+
+    void on_Plus_clicked();
+
+    void on_Pilkku_clicked();
+
+    void on_Miinus_clicked();
+
+    void on_Jako_clicked();
+
+    void on_Kerto_clicked();
+
+    void on_Onyhtakuin_clicked();
+
+    void on_PlusMiinus_clicked();
+
+    void on_Neliojuuri_clicked();
+
+    void on_x2_clicked();
+
+    void on_YksiKauttaX_clicked();
+
+    void on_MC_clicked();
+
+    void on_MR_clicked();
+
+    void on_MS_clicked();
+
+    void on_Mplus_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    Calculator m_calculator;
 };
 #endif // MAINWINDOW_H
