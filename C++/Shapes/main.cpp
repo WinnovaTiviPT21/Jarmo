5a3e3f34cb28b76e1f5482ffc0f88f15c7998834
Maahantuoja <122344190+Maahantuoja@users.noreply.github.com>
2023-02-02 09:17:34 +0200

    Korjattu versio
    - Lisätty kolmas rng muuttuja
    - Lisätty lisäehtoja huolehtimaan ettei suorakulmiosta tule neliötä, eikä ellipsistä ympyrää.

--- C++/Shapes/main.cpp
+++ C++/Shapes/main.cpp
@@ -13,9 +13,11 @@ using namespace std;
 int main()
 {
     // Generating random numbers using Mersenne Twister
-    mt19937 rng{}; // Instantiate a 32-bit Mersenne Twister
-    uniform_int_distribution die1{1, 5}; // Noppa 1 rajoittaa arvot 1-5 välille
-    uniform_int_distribution die2{1, 100}; // Noppa 2 ja 3 rajoittaa arvot 1-100 välille
+    // Instantiate a 32-bit Mersenne Twister
+    mt19937 rng{};
+    // rajaa rng arvojen alueen
+    uniform_int_distribution die1{1, 5};
+    uniform_int_distribution die2{1, 100};
     uniform_int_distribution die3{1, 100};
 
     vector<shared_ptr<Shape>> shapes;
@@ -27,40 +29,35 @@ int main()
         int y = die2(rng);
         int z = die3(rng);
 
-        // varmistaa ettei y ja z ole koskaan saman arvoisia
-        while(y != z){
-            // y = die2(rng);
-            z = die3(rng);
-        }
-        // muodon valinta die 1:n "arpoman" luvun mukaan
-        if(x == 1){
+        // muodon valinta die1:n "arpoman" luvun mukaan
+        if(x == 1 && y != z){
             shared_ptr<Shape> shape(new Rectangle (y, z));
             shapes.push_back(shape);
-            // shape->print();
+            //shape->print();
         }
-        if(x == 2){
+        if((x == 2) || (x == 1 && y == z)){
             shared_ptr<Shape> shape(new Square (y));
             shapes.push_back(shape);
-            // shape->print();
+            //shape->print();
         }
-        if(x == 3){
+        if(x == 3 && y != z){
             shared_ptr<Shape> shape(new Ellipse (y, z));
             shapes.push_back(shape);
-            // shape->print();
+            //shape->print();
         }
-        if(x == 4){
+        if((x == 4) || (x == 3 && y == z)){
             shared_ptr<Shape> shape(new Circle (y));
             shapes.push_back(shape);
-            // shape->print();
+            //shape->print();
         }
         if(x == 5){
             shared_ptr<Shape> shape(new Triangle (y, z));
             shapes.push_back(shape);
-            // shape->print();
+            //shape->print();
         }
     }
 
-    // etsii suurimman shapen arean mukaan
+    // etsii suurimman muodon pinta-alan mukaan
     shared_ptr<Shape> largest = shapes[0];
     for(unsigned int i = 0; i < shapes.size(); i++){
         shared_ptr<Shape> s = shapes[i];
