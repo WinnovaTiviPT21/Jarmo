80536257fd44936f046ce300b8121c9bca73b5ce
Maahantuoja <122344190+Maahantuoja@users.noreply.github.com>
2023-03-07 06:02:33 +0200

    Pohjustusta

---
+++ C++/VoittavaStrategia/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <random>
+#include <chrono>
+#include "peli.h"
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
---
+++ C++/VoittavaStrategia/peli.h
@@ -0,0 +1,42 @@
+#ifndef PELI_H
+#define PELI_H
+
+//#include <QObject>
+#include <iostream>
+#include <random>
+
+class Peli /*: public QObject*/
+{
+    //Q_OBJECT
+    //Q_PROPERTY(int rngNro READ getRngNro NOTIFY rngNroChanged)
+    //Q_PROPERTY(int display READ getDisplay WRITE setDisplay NOTIFY displayChanged)
+    //Q_PROPERTY(int counter READ getCounter NOTIFY counterChanged)
+    //Q_PROPERTY(QString guide READ getGuide NOTIFY guideChanged)
+
+public:
+    Peli();
+
+//public slots:
+    void startClicked();
+    void guessClicked();
+
+    int getRngNro();
+    int getDisplay();
+    int setDisplay(int&);
+    int getCounter();
+    std::string getGuide();
+
+//signals:
+    void rngNroChanged();
+    void displayChanged();
+    void counterChanged();
+    void guideChanged();
+
+private:
+    int m_rngNro = 0;
+    int m_display = 0;
+    int m_counter = 0;
+    std::string m_guide = "Arvaa luku 0 - 100 väliltä.";
+};
+
+#endif // PELI_H
