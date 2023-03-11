8287a156865f08f350de30ba60d61d714e2edf43
Maahantuoja <122344190+Maahantuoja@users.noreply.github.com>
2023-03-11 07:10:07 +0200

    Vektorin täyttäminen jatkuu

---
+++ C++/Vectorin_tayttaminen_jatkuu/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main()
+{
+    mt19937 gen(42);
+    uniform_int_distribution<int> distr;
+
+    const int N = 1e9;
+
+    vector<int> numbers(N); // Varaa N määrän elementtejä arvolla 0.
+    numbers.resize(N);      // Asettaa kooksi N
+    for (int i = 0; i < N; i++) {
+        int randomNumber = distr(gen);
+        numbers[i] = randomNumber;
+    }
+
+    cout << "" << endl;
+    return 0;
+}
