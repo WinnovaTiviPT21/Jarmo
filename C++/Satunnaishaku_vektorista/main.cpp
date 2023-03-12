166439cfd1a471dfbfc9556d2fbe9e1a023215b4
Maahantuoja <122344190+Maahantuoja@users.noreply.github.com>
2023-03-12 04:19:10 +0200

    Update main.cpp

--- C++/Satunnaishaku_vektorista/main.cpp
+++ C++/Satunnaishaku_vektorista/main.cpp
@@ -4,7 +4,26 @@
 
 using namespace std;
 
-
+/*
+**
+** Tehtävänanto
+**
+** Tee konsolisovellus, joka arpoo satunnaislukuja 10 miljoona kappaleita ja tallentaa ne STL vectoriin.
+** Alusta Mersenne Twister seuraavasti:
+**
+** std::mt19937 generator{ 42 };
+** std::uniform_int_distribution<int> distribution{ std::numeric_limits<int>::min(), std::numeric_limits<int>::max() };
+**
+** Hae seuraavat luvut vektorista ja tulosta kaikki kohdat (indeksi), joissa luku esiintyy.
+**
+**  -1245214370
+**   122460941
+**  -122460941
+**   1385283091
+**
+** Jos lukua ei löydy vektorista, tulosta: ei löydy.
+**
+*/
 int main()
 {
     Search mySearch;
