b10b60d22e0518e8d9093564928389f7cda67879
Maahantuoja <122344190+Maahantuoja@users.noreply.github.com>
2023-03-15 11:06:46 +0200

    redy

---
+++ C++/Haku_iteraattoreilla/main.cpp
@@ -0,0 +1,43 @@
+/*
+**
+** Tehtävänanto
+** Tee konsolisovellus, joka arpoo satunnaislukuja miljoona kappaleita ja tallentaa ne STL vectoriin.
+**
+** Alusta Mersenne Twister seuraavasti
+**   std::mt19937 generator{ 42 };
+**   std::uniform_int_distribution<int> distribution{ std::numeric_limits<int>::min(), std::numeric_limits<int>::max() };
+**
+** Tulosta taulukosta elementit väliltä 123456 - 123460 reunat mukaan lukien. Käytä iteraattoreita.
+** Palauta polku GitHub:ssa ja ohjelman tulostus
+**
+*/
+
+#include <iostream>
+#include <random>
+#include <vector>
+#include <iterator>
+
+using namespace std;
+
+int main()
+{
+    const int N = 1e6;
+
+    mt19937 gen{42};
+    uniform_int_distribution<int> distr{numeric_limits<int>::min(), numeric_limits<int>::max()};
+
+    vector<int> numbers(N);
+    for (int i = 0; i < N; i++) {
+        int randomNumber = distr(gen);
+        numbers[i] = randomNumber;
+    }
+
+    vector<int>::iterator it = numbers.begin();
+    for (int i = 123456; i <= 123460; i++) {
+        advance (it, i);
+        cout << *it << endl;
+    }
+
+    cout << "" << endl;
+    return 0;
+}
