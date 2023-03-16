ad86d1ce5c216567afb690bcc72980558a2305e0
Maahantuoja <122344190+Maahantuoja@users.noreply.github.com>
2023-03-16 06:00:16 +0200

    Redy

--- C++/Elementtien_lukumaaran_laskeminen_uudestaan/main.cpp
+++ C++/Elementtien_lukumaaran_laskeminen_uudestaan/main.cpp
@@ -33,9 +33,18 @@
 
 using namespace std;
 
+void counter(int x, vector<int> &numbers) { // ilman & merkkiä antaa ajaksi huomattavasti suuremman ajan?
+    vector<int>::iterator itLow, itUp;
+    itLow = lower_bound (numbers.begin(), numbers.end(), x);
+    itUp = upper_bound (numbers.begin(), numbers.end(), x);
+
+    cout << "Lukuja " << x << " loytyi: " << itUp - itLow << " kpl" << endl;
+}
+
 int main()
 {
     const int N = 1e7;
+    int numA = -1245214370, numB = -164242, numC = 164242, numD = 826750389;
 
     mt19937 gen{42};
     uniform_int_distribution<int> distr{numeric_limits<int>::min(), numeric_limits<int>::max()};
@@ -49,10 +58,33 @@ int main()
 
     sort(numbers.begin(), numbers.end());
 
-//    auto start = std::chrono::steady_clock::now();
+    auto start = std::chrono::steady_clock::now();
 
-//    auto end = std::chrono::steady_clock::now();
-//    cout << "Elapsed time in microseconds: " << chrono::duration_cast<chrono::microseconds>(end - start).count();
+    /*
+    vector<int>::iterator itLow, itUp;
+    itLow = lower_bound (numbers.begin(), numbers.end(), -1245214370);
+    itUp = upper_bound (numbers.begin(), numbers.end(), -1245214370);
+    cout << "Lukuja " << -1245214370 << " loytyi: " << itUp - itLow << " kpl" << endl;
+
+    itLow = lower_bound (numbers.begin(), numbers.end(), -164242);
+    itUp = upper_bound (numbers.begin(), numbers.end(), -164242);
+    cout << "Lukuja " << -164242 << " loytyi: " << itUp - itLow << " kpl" << endl;
+
+    itLow = lower_bound (numbers.begin(), numbers.end(), 164242);
+    itUp = upper_bound (numbers.begin(), numbers.end(), 164242);
+    cout << "Lukuja " << 164242 << " loytyi: " << itUp - itLow << " kpl" << endl;
+
+    itLow = lower_bound (numbers.begin(), numbers.end(), 826750389);
+    itUp = upper_bound (numbers.begin(), numbers.end(), 826750389);
+    cout << "Lukuja " << 826750389 << " loytyi: " << itUp - itLow << " kpl" << endl;
+    */
+    counter(numA, numbers);
+    counter(numB, numbers);
+    counter(numC, numbers);
+    counter(numD, numbers);
+
+    auto end = std::chrono::steady_clock::now();
+    cout << "\nElapsed time in microseconds: " << chrono::duration_cast<chrono::microseconds>(end - start).count() << endl;
 
     cout << "" << endl;
     return 0;
