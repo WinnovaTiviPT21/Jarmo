4463cd9931f95b73bc4fe5ae45401de7b99ef53c
Maahantuoja <122344190+Maahantuoja@users.noreply.github.com>
2023-04-04 13:00:41 +0300

    updatea

--- C++/Usein_yhdessa_ostetut_tuotteet/main.cpp
+++ C++/Usein_yhdessa_ostetut_tuotteet/main.cpp
@@ -1,9 +1,89 @@
 #include <iostream>
+#include <fstream>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
+    ifstream inf{"Market_Basket_Optimisation.csv"};
+    if (!inf)
+    {
+        cerr << "Uh oh, Market_Basket_Optimisation.csv could not be opened for reading!\n\n";
+        return 1;
+    }
+
+    multiset<string> things;
+    multiset<multiset<string>> baskets;
+
+    /*
+    // Ei tulosta kaikkia tietoja, miksi?
+    // Loopilla
+    while(!inf.eof()){
+        string strInput;
+        getline(inf, strInput);
+
+        string word = "";
+        for (auto x : strInput)
+        {
+            if (x == ',')
+            {
+                things.insert(word);
+                word = "";
+            }
+            else {
+                word = word + x;
+            }
+        }
+    }
+    */
+
+    // Setti
+    while(!inf.eof()){
+        string strInput;
+        getline(inf, strInput);
+
+        stringstream sstr(strInput);
+        while(sstr.good())
+        {
+            string substr;
+            getline(sstr, substr, ',');
+            things.insert(substr);
+        }
+        baskets.insert(things);
+    }
+
+//    // Vectori
+//    vector<string> v;
+//    while(!inf.eof()){
+//        string strInput;
+//        getline(inf, strInput);
+
+//        stringstream sstr(strInput);
+//        while(sstr.good())
+//        {
+//            string substr;
+//            getline(sstr, substr, ',');
+//            v.push_back(substr);
+//        }
+//    }
+
+    inf.close();
+
+    // Loopin / Setin tulostus
+    for (set<string>::iterator it = things.begin(); it != things.end(); it++) {
+        cout << *it << endl;
+    }
+
+//    // Vectorin järjestely ja tulostus
+//    sort(v.begin(), v.end());
+//    for (auto x : v) {
+//        cout << x << endl;
+//    }
+
     cout << "" << endl;
     return 0;
 }
