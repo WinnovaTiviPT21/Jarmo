60393af7344a7d2ce4242adc15ceb52fa8d66d9e
Maahantuoja <122344190+Maahantuoja@users.noreply.github.com>
2023-04-11 14:11:22 +0300

    valmis

---
+++ C++/Haku_lompakosta_1/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    string s1 = "20 e";
+    string s2 = "50 cent";
+
+    map<string, int> lompakko1 {{"1 cent", 5}, {"2 cent", 3}, {"5 cent", 5}, {"10 cent", 3},
+                                {"50 cent", 7}, {"1 e", 5}, {"2 e", 3}, {"5 e", 5},
+                                {"10 e", 1}, {"20 e", 5}, {"50 e", 8}, {"100 e", 3}};
+
+    lompakko1.find(s1);
+    lompakko1.find(s2);
+
+    /*
+    // Vaihtoehtonen menetelmä. Kumpi on parempi?
+    cout << "20 e on: " << lompakko1.find(s1)->second << " kpl" << endl;
+    cout << "50 cent on: " << lompakko1.find(s2)->second << " kpl" << endl;
+    */
+    cout << "20 e on: " << lompakko1[s1] << " kpl" << endl;
+    cout << "50 cent on: " << lompakko1[s2] << " kpl" << endl;
+
+    cout << "" << endl;
+    return 0;
+}
