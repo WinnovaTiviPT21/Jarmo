6e70c8e9b59e2b4b30fa600b6b02b76ef1f96971
Maahantuoja <122344190+Maahantuoja@users.noreply.github.com>
2023-04-17 18:31:58 +0300

    Update main.cpp

--- C++/Haku_lompakosta_1/main.cpp
+++ C++/Haku_lompakosta_1/main.cpp
@@ -9,12 +9,11 @@ int main()
     string s2 = "50 cent";
 
     map<string, int> lompakko {{"1 cent", 5}, {"2 cent", 3}, {"5 cent", 5}, {"10 cent", 3},
-                                {"50 cent", 7}, {"1 e", 5}, {"2 e", 3}, {"5 e", 5},
-                                {"10 e", 1}, {"20 e", 5}, {"50 e", 8}, {"100 e", 3}};
+                               {"50 cent", 7}, {"1 e", 5}, {"2 e", 3}, {"5 e", 5},
+                               {"10 e", 1}, {"20 e", 5}, {"50 e", 8}, {"100 e", 3}};
 
     lompakko.find(s1);
     lompakko.find(s2);
-
 
 //    cout << "20 cent on: " << lompakko.find(s1)->second << " kpl" << endl;
 //    cout << "50 cent on: " << lompakko.find(s2)->second << " kpl" << endl;
